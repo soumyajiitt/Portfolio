38fabadc1fe6f9104a882b83ac5fead280ba7d7a
Soumyajit Basak <116637467+soumyajiitt@users.noreply.github.com>
2024-09-16 19:09:48 +0530

    New Testimonial Added

--- components/testimonials/Data.jsx
+++ components/testimonials/Data.jsx
@@ -1,17 +1,25 @@
-import Image1 from "../../src/assets/Testimonial1.png";
-import Image2 from "../../src/assets/Testimonial2.png";
+import Image1 from "../../src/assets/Testimonial3.png"
+;import Image2 from "../../src/assets/Testimonial1.png";
+import Image3 from "../../src/assets/Testimonial2.png";
 
 export const Data = [
     {
       id: 1,
       image: Image1,
+      title: "Rupam Chakraborty",
+      description:
+        "Soumyajit has quickly proven his React expertise since joining our team. His coding skills have already made a significant impact. A valuable asset and a proactive team player, Soumyajit is a great addition to any team.",
+    },
+    {
+      id: 2,
+      image: Image2,
       title: "Venkata Narasimha Reddy Jekkireddy",
       description:
         "He is hardworking and capable of learning things quickly.",
     },
     {
-      id: 2,
-      image: Image2,
+      id: 3,
+      image: Image3,
       title: "Ritu Sharma",
       description:
         "Soumyajit is working with Formulate AI as an intern. I always find him curious about new things and always coming back with ideas to improve products and services. He is also working on growing the Learn Program and in the process, he started his AI Journey. All the best for your upcoming formulations, Soumyajit will be a great asset to any organization.",
