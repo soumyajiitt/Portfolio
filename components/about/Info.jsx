417d57f1e12b9ab9398bfaeedf593970dac90614
Soumyajit Basak <116637467+soumyajiitt@users.noreply.github.com>
2024-10-01 11:42:40 +0530

    New Changes - "Experience Update"

--- components/about/Info.jsx
+++ components/about/Info.jsx
@@ -8,7 +8,7 @@ const Info = () => {
         <i className="bx bx-award about__icon"></i>
 
             <h3 className="about__title">Experience</h3>
-            <span className="about__subtitle">7 Months Working</span>
+            <span className="about__subtitle">9 Months Working</span>
         </div>
 
         <div className="about__box">
